c77a865f71470624c30ba79540e07400a0f839ed
aymen ELFADILI <aitabbou111@gmail.com>
2026-06-10 12:23:52 +0100

    add EntityGraph

--- src/main/java/com/example/HealthCare/repository/PatientRepository.java
+++ src/main/java/com/example/HealthCare/repository/PatientRepository.java
@@ -3,6 +3,7 @@ package com.example.HealthCare.repository;
 import com.example.HealthCare.model.Patient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -20,5 +21,8 @@ public interface PatientRepository extends JpaRepository<Patient , Long> {
     Optional<Patient> findByUsername(String username);
 
     Page<Patient> findByNomContainingIgnoreCase(String nom, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"rendezVousList"})
+     Optional<Patient> findWithRendezVousById(Long id);
 
 }
